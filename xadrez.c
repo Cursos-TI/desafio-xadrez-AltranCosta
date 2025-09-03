5faa73f3cfc4fb3c5a4122e76a4068728f796ef3
Altran Batista da Costa <143886690+AltranCosta@users.noreply.github.com>
2025-09-03 12:38:01 +0000

    foi Implementado a Recursividade

--- xadrez.c
+++ xadrez.c
@@ -1,47 +1,59 @@
 #include <stdio.h>
 
+// Implementando a Recursividade
+void moverTorre(int casas){
+  if (casas > 0){
+    moverTorre(casas - 1);
+    printf("Torre moveu %d casas a Direita\n", casas);
+  }
+}
+
+void moverBispo(int casas){
+  if (casas > 0){
+    moverBispo(casas - 1);
+    printf("O bispo moveu %d  casas para cima a Direita\n", casas);
+  }
+}
+
+void moverRainha(int casas){
+  if (casas > 0){
+    moverRainha(casas - 1);
+    printf("A rainha moveu %d Casas pra Esquerda\n", casas);
+  }
+}
+
 int main(){
-   
-    int bispo = 1; 
-    int rainha = 1;
-    printf("\n **** Bem Vindo ao jogo de Xadrez **** \n");
+  int moveuTorre = 5;
+  int moveuBispo = 5;
+  int moveuRainha = 5;
 
-    // movimentação da Torre com For
-    for (int torre = 1; torre <= 5; torre++)
-    {
-        printf("A Torre moveu  %d casas para Direita\n", torre);
-    }
-    
-    printf("\n =================================\n");
+  printf("\n--- Movimentos ---\n");
 
-   // movimentação do Bispo com while
-    while (bispo <= 5)
-    {
-       printf("O Bispo moveu %d casas para cima e Direta\n", bispo);
-       bispo++;
-    }
-    
-    printf("\n =================================\n");
-    
-    // movimentação d a Rainha com Do While
-    do
-    {
-        printf("A Rainha moveu %d casas a Esqueda\n", rainha);
-        rainha++;
-    } while (rainha <= 8);
+  printf("\nTorre:\n");
+  moverTorre(moveuTorre);// movimentação da Torre
 
-    printf("\n =================================\n");
+  printf("\n ==================================\n");
 
-    // movimento do cavalo em L 
-    for (int cavalo1 = 1; cavalo1 <= 1; cavalo1++){
+  printf("\n Bispo:\n");
+  moverBispo(moveuBispo);// Movimentação do Bispo
+
+    printf("\n ==================================\n");
+
+  printf("\n Rainha:\n");
+  moverRainha(moveuRainha);// Movimentação da Rainha
+
+  printf("\n ==================================\n");
+
+
+  for (int cavalo1 = 1; cavalo1 <= 1; cavalo1++){
         for (int cavalo = 1; cavalo <= 2; cavalo++)
         {
-            printf("O Cavalo moveu %d Casas para Baixo\n", cavalo);
+            printf("O Cavalo moveu %d Casas para Cima\n", cavalo);
         }
-        printf("O Cavalo Moveu %d casas para Esqueda\n", cavalo1);
-    }
-    
-    
-    
-    return 0;
+        printf("O Cavalo Moveu %d casas para Direita\n", cavalo1);
+  }
+
+
+  return 0;
+
 }
